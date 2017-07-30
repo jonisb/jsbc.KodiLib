1cb3dbb0c9c484ee3f083392c01537d70145658d
jonisb <github.com@JsBComputing.se>
2017-07-30 21:06:50 +0200

    Moved OrderedDict import to pythoncompat.py
    As importing OrderedDict is different depending on Python version.

--- KodiLib.py
+++ KodiLib.py
@@ -6,22 +6,12 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
 
 import logging
 
+from pythoncompat import OrderedDict
+
 logger = logging.getLogger(__name__)
 
 __version__ = '0.0.0'
 __all__ = [] # "echo", "surround", "reverse"
-
-logger.info('Importing "OrderedDict" module.')
-try:
-    from collections import OrderedDict
-except ImportError:
-    logger.warning('Failed to import "OrderedDict" module, trying local module.')
-    try:
-        from ordereddict import OrderedDict
-    except ImportError:
-        logger.exception('Failed to import local "OrderedDict" module, aborting.')
-        raise
-logger.info('Importing "OrderedDict" module, done.')
 
 
 class SettingsClass(OrderedDict):
