aa848c696df9ac43d0318219f5c46b8f89271160
jonisb <github.com@JsBComputing.se>
2020-04-23 19:24:50 +0200

    Create test_KodiLib.py
    Adding a basic test to see that the Kodi object is created

---
+++ tests/test_KodiLib.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import unittest
+
+class test_testing(unittest.TestCase):
+    def test_KodiLib_Settings_init(self):
+        #import jsbc.KodiLib as KodiLib
+        import KodiLib as KodiLib
+        Kodi = KodiLib.kodi()
+        assert type(Kodi) == KodiLib.kodi
+        assert Kodi.settings['client']['name'] == 'KodiLib'
